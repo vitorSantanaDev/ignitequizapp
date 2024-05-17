f5ca3d85076eee9f87551c305f6be784e99fb338
vitorSantanaDev <vitorsantana.developer@gmail.com>
2024-05-17 08:05:55 -0300

    feat: adding more animations

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,7 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { View, ScrollView, TouchableOpacity, Alert } from "react-native";
-import { HouseLine } from "phosphor-react-native";
+import {
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Alert,
+  Pressable,
+} from "react-native";
+import { HouseLine, Trash } from "phosphor-react-native";
 
 import { Header } from "../../components/Header";
 import { HistoryCard, HistoryProps } from "../../components/HistoryCard";
@@ -14,12 +20,16 @@ import Animated, {
   SlideInRight,
   SlideOutRight,
 } from "react-native-reanimated";
+import { Swipeable } from "react-native-gesture-handler";
+import { THEME } from "../../styles/theme";
 
 export function History() {
   const [isLoading, setIsLoading] = useState(true);
   const [history, setHistory] = useState<HistoryProps[]>([]);
 
   const { goBack } = useNavigation();
+
+  const swipeableRefs = useRef<Swipeable[]>([]);
 
   async function fetchHistory() {
     const response = await historyGetAll();
@@ -33,7 +43,8 @@ export function History() {
     fetchHistory();
   }
 
-  function handleRemove(id: string) {
+  function handleRemove(id: string, index: number) {
+    swipeableRefs.current?.[index].close();
     Alert.alert("Remover", "Deseja remover esse registro?", [
       {
         text: "Sim",
@@ -64,15 +75,36 @@ export function History() {
         contentContainerStyle={styles.history}
         showsVerticalScrollIndicator={false}
       >
-        {history.map((item) => (
+        {history.map((item, index) => (
           <Animated.View
             key={item.id}
             entering={SlideInRight}
             exiting={SlideOutRight}
             layout={Layout.springify()}
           >
-            <TouchableOpacity onPress={() => handleRemove(item.id)}>
-              <HistoryCard data={item} />
+            <TouchableOpacity onPress={() => handleRemove(item.id, index)}>
+              <Swipeable
+                ref={(ref) => {
+                  const refAlreadyExists = swipeableRefs?.current?.findIndex(
+                    (r) => r === ref
+                  );
+                  if (refAlreadyExists === -1 && ref) {
+                    swipeableRefs.current.push(ref);
+                  }
+                }}
+                leftThreshold={10}
+                onSwipeableOpen={() => handleRemove(item.id, index)}
+                containerStyle={styles.swipeableContainer}
+                overshootLeft={false}
+                renderRightActions={() => null}
+                renderLeftActions={() => (
+                  <View style={styles.swipeableRemove}>
+                    <Trash size={32} color={THEME.COLORS.GREY_100} />
+                  </View>
+                )}
+              >
+                <HistoryCard data={item} />
+              </Swipeable>
             </TouchableOpacity>
           </Animated.View>
         ))}
--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Alert, ScrollView, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 
 import { useNavigation, useRoute } from "@react-navigation/native";
 
@@ -15,12 +15,16 @@ import { ConfirmButton } from "../../components/ConfirmButton";
 import { OutlineButton } from "../../components/OutlineButton";
 import Animated, {
   Easing,
+  Extrapolate,
   interpolate,
+  useAnimatedScrollHandler,
   useAnimatedStyle,
   useSharedValue,
   withSequence,
   withTiming,
 } from "react-native-reanimated";
+import { ProgressBar } from "../../components/ProgressBar";
+import { THEME } from "../../styles/theme";
 
 interface Params {
   id: string;
@@ -38,6 +42,7 @@ export function Quiz() {
   );
 
   const shake = useSharedValue(0);
+  const scrollY = useSharedValue(0);
 
   const shakeAnimtedStyle = useAnimatedStyle(() => ({
     transform: [
@@ -123,6 +128,36 @@ export function Quiz() {
     );
   }
 
+  const fixedProgressBarAnimatedStyle = useAnimatedStyle(() => ({
+    position: "absolute",
+    paddingTop: 50,
+    backgroundColor: THEME.COLORS.GREY_500,
+    width: "110%",
+    left: "-5%",
+    opacity: interpolate(scrollY.value, [60, 90], [0, 1], Extrapolate.CLAMP),
+    transform: [
+      {
+        translateY: interpolate(
+          scrollY.value,
+          [50, 100],
+          [-40, 0],
+          Extrapolate.CLAMP
+        ),
+      },
+    ],
+    zIndex: 1,
+  }));
+
+  const headerStyles = useAnimatedStyle(() => ({
+    opacity: interpolate(scrollY.value, [50, 100], [1, 0], Extrapolate.CLAMP),
+  }));
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      scrollY.value = event.contentOffset.y;
+    },
+  });
+
   useEffect(() => {
     const quizSelected = QUIZ.filter((item) => item.id === id)[0];
     setQuiz(quizSelected);
@@ -141,15 +176,26 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
-      <ScrollView
+      <Animated.View style={fixedProgressBarAnimatedStyle}>
+        <Text style={styles.title}>{quiz.title}</Text>
+        <ProgressBar
+          total={quiz.questions.length}
+          current={currentQuestion + 1}
+        />
+      </Animated.View>
+      <Animated.ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.question}
+        onScroll={scrollHandler}
+        scrollEventThrottle={16}
       >
-        <QuizHeader
-          title={quiz.title}
-          currentQuestion={currentQuestion + 1}
-          totalOfQuestions={quiz.questions.length}
-        />
+        <Animated.View style={[styles.header, headerStyles]}>
+          <QuizHeader
+            title={quiz.title}
+            currentQuestion={currentQuestion + 1}
+            totalOfQuestions={quiz.questions.length}
+          />
+        </Animated.View>
         <Animated.View style={shakeAnimtedStyle}>
           <Question
             key={quiz.questions[currentQuestion].title}
@@ -163,7 +209,7 @@ export function Quiz() {
           <OutlineButton title="Parar" onPress={handleStop} />
           <ConfirmButton onPress={handleConfirm} />
         </View>
-      </ScrollView>
+      </Animated.ScrollView>
     </View>
   );
 }
